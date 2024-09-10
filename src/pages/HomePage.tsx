1686f32a41b3e03d8210dd08052b5340ae247064
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-09-10 11:25:45 +0600

    ok

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 import { FC, useEffect } from "react";
 
 import HomeLayout from "../components/Home";
-import useMessageSocketConnection from "../socketFinal/hooks/useMessageSocketConnection";
-import { messageSocket } from "../socketFinal/namespaces/message.namespace";
+import useMessageSocketConnection from "../socketIo/hooks/useMessageSocketConnection";
+import { messageSocket } from "../socketIo/namespaces/message.namespace";
 
 export const HomePage: FC = (): JSX.Element => {
   const messageSocketState = useMessageSocketConnection();
